ca7d110f321ba5d6df37c4586f4ff41728c1703e
Greg P <88697977+v-9x@users.noreply.github.com>
2024-10-22 10:27:04 -0700

    Start Loops lab

--- Java Class/src/Main.java
+++ Java Class/src/Main.java
@@ -4,5 +4,26 @@ public class Main {
     public static void main(String[] args) {
         // Binary Search
         // Array must be sorted first
+        boolean tFound = false;
+        int[] myArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // set target, hi, lo, and mid
+        int t = 4;
+        int l = 0;
+        int h = myArray.length - 1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (myArray[m] == t) {
+                System.out.println("Target found at: " + m);
+                tFound = true;
+                break;
+            } else if (myArray[m] < t) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        if (!tFound) {
+            System.out.println("Target not found.");
+        }
     }
 }
---
+++ LoopsLab/src/CSCD210LoopsLab.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CSCD210LoopsLab {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a postiive integer to analyze: ");
+        int userNumber = input.nextInt();
+        if (userNumber <= 0) {
+            System.out.println("Number must be a positive integer");
+        }
+        while (userNumber > 0) {
+            System.out.println("1. Enter a new number");
+            System.out.println("2. Print the number of odd digits, even digits and zeros in the integer");
+            System.out.println("3. Print the prime numbers between 2 and the integer (inclusive)");
+            System.out.println("4. Quit the program");
+            System.out.println("Please enter your choice: ");
+            int choice = input.nextInt();
+            char[] myNumArr = Integer.toString(userNumber).toCharArray();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a new positive integer to analyze: ");
+                    userNumber = input.nextInt();
+                    break;
+                case 2:
+                    for (int i = 0; i < myNumArr.length; i++) {
+                        short digit = (short) (myNumArr[i] - '0');
+                        System.out.println(digit);
+                    }
+                    break;
+                case 3:
+
+            }
+        }
+    }
+}
