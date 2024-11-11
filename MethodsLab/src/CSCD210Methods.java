8981d7fcd26e3637af162c75f33738d6c4a35a49
Greg P <88697977+v-9x@users.noreply.github.com>
2024-11-11 10:45:03 -0800

    Add menu

--- MethodsLab/src/CSCD210Methods.java
+++ MethodsLab/src/CSCD210Methods.java
@@ -15,7 +15,7 @@ public class CSCD210Methods {
             throw new IllegalArgumentException("Number cannot be less than 1 and input cannot be null.");
         }
         double avgDigits = findAvgOfDigits(theNum);
-        System.out.println("Please input a second number: ");
+        System.out.print("Please input a second number: ");
         int inputNum = kb.nextInt();
         double secondAvgDigits = findAvgOfDigits(inputNum);
         String lightOrHeavy = avgDigits > secondAvgDigits ? "Heavy" : "Light";
@@ -31,5 +31,23 @@ public class CSCD210Methods {
             inputNum = kb.nextInt();
         } while (inputNum <= 0);
         return inputNum;
+    }
+    public static int menu(Scanner kb){
+        if (kb == null) {
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+        int input;
+        do{
+            System.out.println("Menu Choices are: ");
+            System.out.println("1) Enter a new number");
+            System.out.println("2) Print the number of even/odd/zeros");
+            System.out.println("3) Print if the number is light or heavy");
+            System.out.println("4) Print the value of E");
+            System.out.println("5) Print the number in reverse order");
+            System.out.println("6) Quit");
+            System.out.print("Please enter your choice: ");
+            input = kb.nextInt();
+        } while(input < 1 || input > 6);
+        return input;
     }
 }
--- MethodsLab/src/CSCD210MethodsMain.java
+++ MethodsLab/src/CSCD210MethodsMain.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class CSCD210MethodsMain {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        CSCD210Methods.readPosNum(kb);
+//        CSCD210Methods.readPosNum(kb);
 //        CSCD210Methods.lightOrHeavy(4456,kb);
+        CSCD210Methods.menu(kb);
     }
 }
