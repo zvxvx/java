6baeaa7971d82f4d40d92f1e863c861eeb0d5892
Greg P <88697977+v-9x@users.noreply.github.com>
2024-11-13 13:53:09 -0800

    Add computer class to play with

---
+++ Java Class/src/computer/Computer.java
@@ -0,0 +1,54 @@
+package computer;
+
+public class Computer {
+    //numbers based on gigabytes or cpu cores
+    private int memory;
+    private int storage;
+    private int cpu;
+
+    // DVC
+    public Computer(){
+        this.memory = 32;
+        this.storage = 1_000;
+        this.cpu = 8;
+    }
+
+    //EVC
+    public Computer(int mem, int store, int cpu){
+        this.memory = mem;
+        this.storage = store;
+        this.cpu = cpu;
+    }
+
+    // Setters
+    public void setComputerMemory(int m){
+        this.memory = m;
+    }
+    public void setComputerStorage(int s){
+        this.storage = s;
+    }
+    public void setComputerCpu(int c){
+        this.cpu = c;
+    }
+
+    //Getters
+    public int getComputerMemory(){
+        return this.memory;
+    }
+    public int getComputerStorage(){
+        return this.storage;
+    }
+    public int getComputerCpu(){
+        return this.cpu;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This computer has " + this.memory + " gigs of memory, " + this.storage + " gigs of storage, and " + this.cpu + " cpu cores.");
+    }
+
+    @Override
+    public int hashCode() {
+        return;
+    }
+}
