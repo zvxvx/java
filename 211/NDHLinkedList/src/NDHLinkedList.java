479cb87139ccdac727716cee8bafd8ed2fe7c9a0
Greg P <88697977+v-9x@users.noreply.github.com>
2025-02-04 13:13:32 -0800

    Update project skeleton

---
+++ 211/NDHLinkedList/src/NDHLinkedList.java
@@ -0,0 +1,64 @@
+public class NDHLinkedList implements NDHLinkedListInterface {
+    int size;
+    Node head;
+
+    public NDHLinkedList() {
+        this.size = 0;
+        this.head = null;
+    }
+
+    @Override
+    public void addFirst(int num) {
+
+    }
+
+    @Override
+    public void addLast(int num) {
+
+    }
+
+    @Override
+    public void removeFirst() throws IllegalStateException {
+
+    }
+
+    @Override
+    public void removeLast() throws IllegalStateException {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public int search(int num) throws IllegalStateException {
+        return 0;
+    }
+
+    @Override
+    public Node getAtIndex(int i) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void removeAtIndex(int i) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void addAtIndex(int i, int data) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void printList() {
+
+    }
+}
---
+++ 211/NDHLinkedList/src/NDHLinkedListInterface.java
@@ -0,0 +1,74 @@
+public interface NDHLinkedListInterface {
+    /**
+     * Add a node to the beginning of the list
+     * @param num - our data
+     */
+    void addFirst(int num);
+
+    /**
+     * Add a node to the end of the list.
+     * @param num - our data
+     */
+    void addLast(int num);
+
+    /**
+     * Remove the first node in the list.
+     * @throws IllegalStateException - if the list is empty
+     */
+    void removeFirst() throws IllegalStateException;
+
+    /**
+     * Remove the last node in the list.
+     * @throws IllegalStateException - if the list is empty
+     */
+    void removeLast() throws IllegalStateException;
+
+    /**
+     *
+     * @return the string in its proper form.
+     */
+    String toString();
+
+    /**
+     * Searches the list for the first occurrence of the node containing the
+     * data.
+     * @param num - the value to be found in the list.
+     * @return - the location of the data searched.
+     * @throws IllegalStateException - if the list is empty
+     */
+    int search(int num) throws IllegalStateException;
+
+    /**
+     *  Get a node at a certain index.
+     * @param i - the index we want to search with.
+     * @return the node we want.
+     * @throws IllegalArgumentException - if index is oob or list is empty.
+     */
+    Node getAtIndex(int i)throws IllegalArgumentException;
+
+    /**
+     * Remove a node at a certain index.
+     * @param i - the index we want to search with.
+     * @throws IllegalArgumentException - if index is oob or list is empty.
+     */
+    void removeAtIndex(int i)throws IllegalArgumentException;
+
+    /**
+     * Add a node at a certain index.
+     * @param i - the index we want to use to position the node.
+     * @param data - the data we want to use for the node.
+     * @throws IllegalArgumentException - if index is oob.
+     */
+    void addAtIndex(int i, int data)throws IllegalArgumentException;
+
+    /**
+     * Lets us know if a list is empty or not.
+     * @return - a true or false value
+     */
+    boolean isEmpty();
+
+    /**
+     * Prints a list of the nodes.
+     */
+    void printList();
+}
