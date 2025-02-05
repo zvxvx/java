51c022714420c20d46ce1314a75b07af767ed0a4
Greg P <88697977+v-9x@users.noreply.github.com>
2025-02-05 14:51:42 -0800

    Update lab

--- 211/NDHLinkedList/src/NDHLinkedList.java
+++ 211/NDHLinkedList/src/NDHLinkedList.java
@@ -9,21 +9,39 @@ public class NDHLinkedList implements NDHLinkedListInterface {
 
     @Override
     public void addFirst(int num) {
+        this.head = new Node(num, this.head);
         this.size++;
     }
 
     @Override
     public void addLast(int num) {
+        Node last = new Node(num, null);
+        if (this.head == null) {
+            this.head = last;
+            this.size++;
+            return;
+        }
+        Node current = this.head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = last;
         this.size++;
     }
 
     @Override
     public void removeFirst() throws IllegalStateException {
+        this.head.next = this.head;
         this.size--;
     }
 
     @Override
     public void removeLast() throws IllegalStateException {
+        Node current = this.head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current = null;
         this.size--;
     }
 
@@ -59,6 +77,7 @@ public class NDHLinkedList implements NDHLinkedListInterface {
 
     @Override
     public void printList(NDHLinkedList ll) {
+        Node current = this.head;
         System.out.println(ll);
     }
 
