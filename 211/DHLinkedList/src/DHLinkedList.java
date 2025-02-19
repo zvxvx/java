a98ac8ec2dc48f3cc7d9f9ac672823313f6e36e6
Greg P <88697977+v-9x@users.noreply.github.com>
2025-02-18 21:33:38 -0800

    Update project

--- 211/DHLinkedList/src/DHLinkedList.java
+++ 211/DHLinkedList/src/DHLinkedList.java
@@ -1,4 +1,4 @@
-public class DHLinkedList {
+public class DHLinkedList implements DHLinkedListInterface {
     int size;
     Node head;
 
@@ -7,7 +7,48 @@ public class DHLinkedList {
         this.head = null;
     }
 
-    public int getSize() {
-        return this.size;
+    @Override
+    public void addFirst(String data) {
+
+    }
+
+    @Override
+    public void addLast(String data) {
+
+    }
+
+    @Override
+    public void removeFirst() throws IllegalStateException {
+
+    }
+
+    @Override
+    public void removeLast() throws IllegalStateException {
+
+    }
+
+    @Override
+    public int search(String data) throws IllegalStateException {
+        return 0;
+    }
+
+    @Override
+    public Node getAtIndex(int index) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void removeAtIndex(int index) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void addAtIndex(int index) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 }
